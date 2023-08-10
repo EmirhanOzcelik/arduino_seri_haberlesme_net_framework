3791ac9291de3889b598da89eab5bd7a3a52d028
EmirhanOzcelik <94894644+EmirhanOzcelik@users.noreply.github.com>
2023-08-10 09:37:58 +0300

    Add files via upload

---
+++ seri_haberlesme_son/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.port1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // port1
+            // 
+            this.port1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TabStop = false;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(943, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Emirhan Özçelik";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(502, 252);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "*exit",
+            "*clear",
+            "*timeon",
+            "*timeoff",
+            "*println",
+            "*print"});
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.Location = new System.Drawing.Point(13, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.TabStop = false;
+            this.button2.Text = "enter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(389, 302);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 20);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "zaman damgası";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(140, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 20);
+            this.button1.TabIndex = 9;
+            this.button1.TabStop = false;
+            this.button1.Text = "bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.CheckedChanged += new System.EventHandler(this.button1_baglan);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 448);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort port1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox button1;
+    }
+}
+
---
+++ seri_haberlesme_son/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics.Eventing.Reader;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        int saat = DateTime.Now.Hour;
+        int dk = DateTime.Now.Minute;
+        int sn = DateTime.Now.Second;
+        string e = Environment.NewLine;
+        bool port_durum = false;
+        bool makine_kaydir = false;
+        bool ozel_mesaj = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void yaz(String v)
+        {
+            if (checkBox1.Checked)
+            {
+                saat = DateTime.Now.Hour;
+                dk = DateTime.Now.Minute;
+                sn = DateTime.Now.Second;
+                textBox1.AppendText(e+saat+":"+dk+":"+sn+"  --> "+ v + e);
+            }
+            else
+            {
+                textBox1.AppendText(e + v + e);
+            }
+            
+        }
+        private void Alln(string vv)
+        {
+            if (checkBox1.Checked)
+            {
+                saat = DateTime.Now.Hour;
+                dk = DateTime.Now.Minute;
+                sn = DateTime.Now.Second;
+                textBox1.AppendText(e +saat+":"+dk+":"+sn+"  | makine --> "+ vv + e);
+            }
+            else
+            {
+                textBox1.AppendText(e + " makine --> " + vv + e);
+            } 
+        }
+        private void Al(String vv)
+        {
+                textBox1.AppendText(vv);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "bağlan";
+            button1.BackColor = Color.White;
+            textBox1.ReadOnly = true;
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Both;
+            button1.Enabled = false;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_baglan(object sender, EventArgs e)
+        { 
+            
+            if(comboBox1.SelectedItem != null && !port1.IsOpen && !port_durum)
+            {
+                try
+                {
+                    port1.PortName = comboBox1.SelectedItem.ToString();
+                    port1.Open();
+                    if(port1.IsOpen)
+                    {
+                        button1.BackColor = Color.Green;
+                        button1.Text = "kes";
+                        port_durum = true;
+                        yaz("bağlantı başlatıldı");
+                        button1.CheckState = CheckState.Checked;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if(port_durum == true)
+            {
+                try
+                {
+                    port1.Close();
+                    if (!port1.IsOpen)
+                    {
+                        button1.CheckState = CheckState.Unchecked;
+                        button1.BackColor = Color.White;
+                        button1.Text = "bağlan";
+                        port_durum = false;
+                        yaz("bağlantı kesildi");
+                        button1.CheckState = CheckState.Unchecked;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            } 
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            String[] ports = SerialPort.GetPortNames();
+            foreach (String port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (port1.IsOpen)
+                {
+                    port1.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string girilen = textBox2.Text;
+            string aranan = "";
+            if(girilen.Length >= 1)
+            {
+                yaz(textBox2.Text);
+                aranan = girilen.Substring(0,1);
+            }
+            if (aranan == "*")
+            {
+                ozel_mesaj = true;
+                if (textBox2.Text == "*exit")
+                {
+                    Close();
+                }
+                if (textBox2.Text == "*clear")
+                {
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+                if (textBox2.Text == "*timeon")
+                {
+                    checkBox1.Checked = true;
+                }
+                if (textBox2.Text == "*timeoff")
+                {
+                    checkBox1.Checked = false;
+                }
+                if (textBox2.Text == "*println")
+                {
+                    makine_kaydir = true;
+                }
+                if (textBox2.Text == "*print")
+                {
+                    makine_kaydir = false;
+                } 
+            }
+            else
+            {
+                ozel_mesaj = false;
+            }
+            if (port1.IsOpen)
+            {
+                if (!ozel_mesaj)
+                {
+                    string veri = textBox2.Text.ToString();
+                    port1.Write(veri);
+                }
+               
+            }
+            textBox2.Clear();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button2_Click(sender, e);
+            }
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void port1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string veri = port1.ReadExisting();
+            try
+            {
+                if (makine_kaydir)
+                {
+                    Alln(veri);
+                }
+                else
+                {
+                    Al(veri);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
